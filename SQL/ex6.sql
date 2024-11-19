0621882ae86a7ba7d46ca82f7fd075173631f9c1
sarah888 <shuan392@uwo.ca>
2024-11-19 11:46:05 -0500

    update ex 6 and 7

--- SQL/ex6.sql
+++ SQL/ex6.sql
@@ -1,1 +1,34 @@
 -- select statements from Item 5;
+DELETE FROM Injury
+WHERE total_days_injured < 15
+AND player_ID IN (
+    SELECT A.player_ID
+    FROM Athlete A
+    JOIN Team T ON A.team_ID = T.team_ID
+    WHERE T.location IN ('Curtisstad', 'Reedport')
+);
+
+
+UPDATE Team
+SET wins = wins + (
+    SELECT COUNT(*) 
+    FROM Statistic S
+    JOIN Athlete A ON S.player_ID = A.player_ID
+    WHERE A.team_ID = Team.team_ID AND S.points > 20
+)
+WHERE team_ID = 1;
+
+
+
+
+INSERT INTO Injury (player_ID, injury_type, injury_date, return_date, total_days_injured, status)
+SELECT 
+    A.player_ID, 
+    'Fatigue', 
+    CURDATE(), 
+    DATE_ADD(CURDATE(), INTERVAL 10 DAY), 
+    10, 
+    'Recovering'
+FROM Athlete A
+JOIN Team T ON A.team_ID = T.team_ID
+WHERE T.record > 0.8;
--- SQL/ex7.sql
+++ SQL/ex7.sql
@@ -1,1 +1,30 @@
 --  create view statements from Item 7; 
+CREATE VIEW TopTeamStatistics AS
+SELECT 
+    team_ID,
+    team_name,
+    league,
+    wins,
+    losses,
+    record,
+    standing
+FROM Team
+WHERE record > 0.75;
+
+CREATE VIEW PlayerInjuryDetails AS
+SELECT 
+    A.player_ID,
+    A.player_name,
+    T.team_name,
+    I.injury_type,
+    I.injury_date,
+    I.return_date,
+    I.total_days_injured,
+    I.status
+FROM Athlete A
+JOIN Team T ON A.team_ID = T.team_ID
+JOIN Injury I ON A.player_ID = I.player_ID;
+
+
+INSERT INTO TopTeamStatistics (team_ID, team_name, league, wins, losses, record, standing)
+VALUES (10, 'Western Warriors', 'Western League', 10, 2, 0.83, 5);
